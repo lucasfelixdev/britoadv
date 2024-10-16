ca0226b6c0d948736fd997899cb8ed965a4d91e4
Lukas Félix <lucas2009jesus@gmail.com>
2024-10-16 20:56:51 -0300

    UPDATE IMGS

--- script/responsividade.js
+++ script/responsividade.js
@@ -1,3 +1,17 @@
+
+
+function toggleBackgroundColor(element) {
+  // Detecta se a tela está abaixo de 768px (dispositivos móveis)
+  if (window.innerWidth <= 768) {
+
+
+      if (element.style.backgroundColor === 'rgb(255 255 255 / 0%)') { // Verifica se a cor é a cor alvo
+     
+          element.style.backgroundColor = '#ebc894';
+      }
+  }
+}
+
 let show = true;
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = document.querySelector(".menu-toggle")
@@ -7,15 +21,7 @@ const navBar = document.querySelector(".nav-bar__container")
 menuToggle.addEventListener("click", () =>{
     document.body.style.overflow = show ? "hidden" : "initial"
     menuSection.classList.toggle("on", show)
-    navBar.classList.toggle("on")
+    navBar.classList.toggle("on", show)
     show = !show;
 })
 
-itens.forEach(item => {
-  item.addEventListener("click", () => {
-    menuSection.classList.remove("on");
-    navBar.classList.remove("on")
-    document.body.style.overflow = "initial";
-    show = true;
-  });
-});
